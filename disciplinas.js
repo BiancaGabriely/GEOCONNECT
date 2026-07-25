ecc4dc5865b7e7b0f435dc4d03f8c200c514f906
BiancaGabriely <bianca.gabriely@escolar.ifrn.edu.br>
2026-07-25 14:16:45 -0300

    Fizalização da página disciplinas e início da página principal

---
+++ disciplinas.js
@@ -0,0 +1,69 @@
+const disciplinas = [
+    {
+        nome: "Mineralogia",
+        descricao: "Materiais voltados ao estudo dos minerais.",
+        materiais: 34,
+        questoes: 78,
+        acessos: 150,
+        professores: 3
+    },
+    {
+        nome: "Topografia",
+        descricao: "Materiais sobre levantamento topográfico.",
+        materiais: 20,
+        questoes: 45,
+        acessos: 89,
+        professores:7
+    }
+];
+
+const quantidade = document.getElementById("quantidade-disciplinas");
+
+quantidade.textContent = disciplinas.length;
+const lista = document.getElementById("lista-disciplinas");
+
+disciplinas.forEach(disciplina => {
+    lista.innerHTML += `
+    <div class="card-disciplina">
+
+        <div class="info">
+
+            <div class="icone">
+             <i class="bi bi-book"></i>
+            </div>
+
+            <div>
+
+                <div class="titulo">${disciplina.nome}</div>
+
+                <div class="descricao">${disciplina.descricao}</div>
+
+            </div>
+
+        </div>
+        <div class="estatisticas">
+
+            <div>
+                <strong>${disciplina.materiais}</strong>
+                <span>materiais</span>
+            </div>
+
+            <div>
+                <strong>${disciplina.questoes}</strong>
+                <span>questões</span>
+            </div>
+
+            <div>
+                <strong>${disciplina.professores}</strong>
+                <span>professores</span>
+            </div>
+        </div>
+
+        <div class="seta">
+            <i class="bi bi-chevron-right"></i>
+        </div>
+
+    </div>
+    `;
+});
+    

010ebb8e99d3ff4b661578e5b18a12988bc79c25
BiancaGabriely <bianca.gabriely@escolar.ifrn.edu.br>
2026-07-27 15:49:39 -0300

    Nova estruturação de pastas e finalização da página index

---
+++ index/dados.js
@@ -0,0 +1,123 @@
+//dados
+
+const dashboard = {
+
+    materiais: 142,
+
+    disciplinas: 9,
+
+    professores: 18,
+
+    questoes: 356
+
+};
+
+
+
+const disciplinas = [
+
+
+
+    {
+
+        nome: "Geologia",
+
+        descricao: "Materiais de Geologia",
+
+        materiais: 25,
+
+        icone: "bi-globe-americas"
+
+    },
+
+
+
+    {
+
+        nome: "Mineralogia",
+
+        descricao: "Materiais de Mineralogia",
+
+        materiais: 32,
+
+        icone: "bi-gem"
+
+    },
+
+
+
+    {
+
+        nome: "Topografia",
+
+        descricao: "Materiais de Topografia",
+
+        materiais: 18,
+
+        icone: "bi-rulers"
+
+    },
+
+
+
+    {
+
+        nome: "Petrologia",
+
+        descricao: "Materiais de Petrologia",
+
+        materiais: 14,
+
+        icone: "bi-box"
+
+    }
+
+
+
+];
+
+
+
+const materiaisRecentes = [
+
+
+
+    {
+
+        titulo:"Mapa Geológico",
+
+        disciplina:"Geologia",
+
+        data:"Hoje"
+
+    },
+
+
+
+    {
+
+        titulo:"Minerais Industriais",
+
+        disciplina:"Mineralogia",
+
+        data:"Ontem"
+
+    },
+
+
+
+    {
+
+        titulo:"Topografia Básica",
+
+        disciplina:"Topografia",
+
+        data:"2 dias atrás"
+
+    }
+
+
+
+];
+
+
